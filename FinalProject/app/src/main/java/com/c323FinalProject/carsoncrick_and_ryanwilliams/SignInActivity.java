969b603f10fa617d9e56e9bca5e3a3bf0ea92b1a
Carson Crick <ccrick@iu.edu>
2020-12-10 01:16:53 -0500

    First Save of Project
    Completed UI for Sign In Activity and Home Activity (not including fragments UI)

---
+++ FinalProject/app/src/main/java/com/c323FinalProject/carsoncrick_and_ryanwilliams/SignInActivity.java
@@ -0,0 +1,23 @@
+package com.c323FinalProject.carsoncrick_and_ryanwilliams;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+
+    //create user with credentials or check credentials
+    public void login(View view) {
+
+    }
+    //method to create dialog for selecting user image
+    public void createUserImage(View view) {
+    }
+}
